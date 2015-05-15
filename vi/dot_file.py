0b6d14a922e0e65352c6edd1d55a04eefaa8dbda
Cameron Chrisman <cchrisma@gmail.com>
2015-05-15 11:01:06 -0700

    .vintageousrc now reads :map or map as a starting token, and now accepts omap, vmap, nmap without errors

--- vi/dot_file.py
+++ vi/dot_file.py
@@ -23,7 +23,7 @@ class DotFile(object):
                 for line in f:
                     cmd, args = self.parse(line)
                     if cmd:
-                        _logger().info('[DotFile] running: {0} {1}'.format(cmd, args))
+                        print('[DotFile] running: {0} {1}'.format(cmd, args))
                         sublime.active_window().run_command(cmd, args)
         except FileNotFoundError:
             pass
@@ -31,20 +31,20 @@ class DotFile(object):
     def parse(self, line):
         try:
             _logger().info('[DotFile] parsing line: {0}'.format(line))
-            if line.startswith((':map ')):
-                line = line[1:]
+            line = line.lstrip(':')
+            if line.startswith(('map ')):
                 return ('ex_map', {'command_line': line.rstrip()})
 
-            if line.startswith((':omap ')):
-                line = line[len(':omap '):]
-                return ('ex_omap', {'cmd': line.rstrip()})
+            if line.startswith(('omap ')):
+                return ('ex_omap', {'command_line': line.rstrip()})
 
-            if line.startswith((':vmap ')):
-                line = line[len(':vmap '):]
-                return ('ex_vmap', {'cmd': line.rstrip()})
+            if line.startswith(('nmap ')):
+                return ('ex_nmap', {'command_line': line.rstrip()})
+            
+            if line.startswith(('vmap ')):
+                return ('ex_vmap', {'command_line': line.rstrip()})
 
-            if line.startswith((':let ')):
-                line = line[1:]
+            if line.startswith(('let ')):
                 return ('ex_let', {'command_line': line.strip()})
         except Exception:
             print('Vintageous: bad config in dotfile: "%s"' % line.rstrip())
